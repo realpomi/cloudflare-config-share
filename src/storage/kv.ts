db7e56aa2fdc1cfb81747e89470a9280833ff784
Realpomi <realpomi@gmail.com>
2023-04-07 13:04:00 +0900

    remove redis

--- src/storage/kv.ts
+++ src/storage/kv.ts
@@ -1,15 +1,15 @@
 import { dev } from "$app/environment";
-import { createClient } from "redis";
+// import { createClient } from "redis";
 
-const client = createClient();
+// const client = createClient();
 
 const KV = {
     get: async(platform:any, key: string) => {
         let k:any = "";
         if( dev ) {
-            await client.connect();
-            k = await client.get(key);            
-            await client.disconnect();
+            // await client.connect();
+            // k = await client.get(key);            
+            // await client.disconnect();
         } else {
             k = await platform?.env.ENV_DATA.get(key);
         }
@@ -18,9 +18,9 @@ const KV = {
     },
     put: async(platform:any, key: string, value: string) => {
         if( dev ) {
-            await client.connect();
-            await client.set(key, value);            
-            await client.disconnect();
+            // await client.connect();
+            // await client.set(key, value);            
+            // await client.disconnect();
         } else {
             await platform?.env.ENV_DATA.put(key, value);
         }
